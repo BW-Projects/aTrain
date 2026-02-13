a81ab6fd3639b91fd6e7703a573a082ec5dfd6ce
Jürgen Fleiß <juergen.fleiss@uni-graz.at>
2026-02-13 09:19:00 +0100

    change model dir path to follow flaptak guideline

--- aTrain_core/globals.py
+++ aTrain_core/globals.py
@@ -1,13 +1,17 @@
+import os
 from importlib.resources import files
 from importlib.util import find_spec
 from pathlib import Path
 from typing import cast
 
-from platformdirs import user_documents_path
+from platformdirs import user_data_path, user_documents_path
 
+FLATPAK = bool(os.environ.get("FLATPAK_ID"))
 ATRAIN_DIR = user_documents_path() / "aTrain"
-MODELS_DIR = ATRAIN_DIR / "models"
-if find_spec("aTrain"):
+MODELS_DIR = (user_data_path() / "models") if FLATPAK else (ATRAIN_DIR / "models")
+if FLATPAK:
+    REQUIRED_MODELS_DIR = MODELS_DIR
+elif find_spec("aTrain"):
     REQUIRED_MODELS_DIR = cast(Path, files("aTrain") / "required_models")
 else:
     REQUIRED_MODELS_DIR = MODELS_DIR
