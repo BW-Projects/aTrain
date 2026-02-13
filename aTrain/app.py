865b4291a31cdfa8d1ab6a5e31998a6df28f752b
Jürgen Fleiß <juergen.fleiss@uni-graz.at>
2026-02-13 09:28:12 +0100

    change settings storage path according to flatpak guideline

--- aTrain/app.py
+++ aTrain/app.py
@@ -4,13 +4,18 @@ from pathlib import Path
 from typing import cast
 from unittest.mock import patch
 
-from aTrain_core.globals import ATRAIN_DIR, REQUIRED_MODELS
+from aTrain_core.globals import ATRAIN_DIR, FLATPAK, REQUIRED_MODELS
 from aTrain_core.load_resources import get_model
+from platformdirs import user_config_path
 from typer import Option, Typer
 from typing_extensions import Annotated
 from wakepy import keep
 
-with patch.dict(os.environ, NICEGUI_STORAGE_PATH=str(ATRAIN_DIR / "settings")):
+NICEGUI_STORAGE_PATH = (
+    user_config_path() / "aTrain" if FLATPAK else (ATRAIN_DIR / "settings")
+)
+
+with patch.dict(os.environ, NICEGUI_STORAGE_PATH=str(NICEGUI_STORAGE_PATH)):
     from nicegui import ui
 
     from aTrain.pages import about, archive, faq, models, transcribe  # noqa
