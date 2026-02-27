80e809c8062e5581978603215c18023818e633d6
Jürgen Fleiß <juergen.fleiss@uni-graz.at>
2026-02-27 21:14:53 +0100

    use same logic of model dir = req model dir (now not in user dir) as flatpak for snap

--- aTrain_core/globals.py
+++ aTrain_core/globals.py
@@ -24,8 +24,8 @@ if SNAP and os.environ.get("ATRAIN_USER_DIR"):
     ATRAIN_DIR = Path(os.environ["ATRAIN_USER_DIR"])
 else:
     ATRAIN_DIR = user_documents_path() / "aTrain"
-MODELS_DIR = (user_data_path() / "models") if FLATPAK else (ATRAIN_DIR / "models")
-if FLATPAK:
+MODELS_DIR = (user_data_path() / "models") if (FLATPAK or SNAP) else (ATRAIN_DIR / "models")
+if FLATPAK or SNAP:
     REQUIRED_MODELS_DIR = MODELS_DIR
 elif find_spec("aTrain"):
     REQUIRED_MODELS_DIR = cast(Path, files("aTrain") / "required_models")
