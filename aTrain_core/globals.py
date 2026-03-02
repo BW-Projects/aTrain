36d56549b10950585e4426dae46ebf447b1aa779
Jürgen Fleiß <juergen.fleiss@uni-graz.at>
2026-03-02 22:16:43 +0100

    adapt flatpak model dirs for bundling required models in build process

--- aTrain_core/globals.py
+++ aTrain_core/globals.py
@@ -20,12 +20,25 @@ if SNAP or (FLATPAK and platform.machine().lower() not in {"aarch64", "arm64"}):
         mp.set_start_method("spawn", force=True)
     except RuntimeError:
         pass
+
+# snapcraft directories
 if SNAP and os.environ.get("ATRAIN_USER_DIR"):
     ATRAIN_DIR = Path(os.environ["ATRAIN_USER_DIR"])
 else:
     ATRAIN_DIR = user_documents_path() / "aTrain"
-MODELS_DIR = (user_data_path() / "models") if (FLATPAK or SNAP) else (ATRAIN_DIR / "models")
-if FLATPAK or SNAP:
+MODELS_DIR = (
+    (user_data_path() / "models") if (FLATPAK or SNAP) else (ATRAIN_DIR / "models")
+)
+
+# flatpak directories.
+if FLATPAK:
+    flatpak_required_models_dir = Path("/app/share/atrain-required-models")
+    REQUIRED_MODELS_DIR = (
+        flatpak_required_models_dir
+        if flatpak_required_models_dir.is_dir()
+        else MODELS_DIR
+    )
+elif SNAP:
     REQUIRED_MODELS_DIR = MODELS_DIR
 elif find_spec("aTrain"):
     REQUIRED_MODELS_DIR = cast(Path, files("aTrain") / "required_models")
